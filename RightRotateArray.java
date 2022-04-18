77d55fb7b50f69c46e80da91c74484277e6e1085
varsha0501 <100074545+varsha0501@users.noreply.github.com>
2022-04-18 09:50:38 +0530

    Updated code with store rotated array in 2D array
    18/04/2022

--- RightRotateArray.java
+++ RightRotateArray.java
@@ -2,37 +2,48 @@ import java.lang.*;
 import java.util.*;
 
 
-public class RightRotateArray {
+public class RightRotateMatrixArray {
     public static void main(String[] args) {
 
-            Scanner sc = new Scanner(System.in);
-            int N = sc.nextInt();
-            int arr[] = new int[N] ;
+        int A[] = {1,2,3,4,5} ;
+        int B[] = {2,3};
+        int temp[];
+        int ans[][] = new int[B.length][A.length];
+        int copiedArray[]  = Arrays.copyOf(A, A.length);
+        for(int i=0;i<B.length;i++) {
+            temp = rotate(A, B[i]);
 
-            int j ;
-            for(int i=0;i<arr.length;i++){
-                arr[i] = sc.nextInt();
+            for (int k = 0; k < A.length; k++) {
+                ans[i][k] = temp[k];
             }
-            int k = sc.nextInt();
-            for(int i=0;i<k;i++){
-                rotate(arr);
-            }
-
-            for(int i=0;i<arr.length;i++){
-                System.out.print(arr[i]+" ") ;
-            }
+            A = Arrays.copyOf(copiedArray,copiedArray.length) ;
+            //System.out.println(ans.length);
+        }
+        printArr(ans); 
 
     }
+    
+    public static void printArr(int[][] arr){
 
-    public static void rotate(int arr[]){
-        // System.out.print("rotate called---") ;
-        int j = 0 ;
-        int last = arr[arr.length-1];
-        for(j=0;j<arr.length-1;j++){
-            arr[j+1] = arr[j] ;
+            for(int i=0;i<arr.length;i++){
+                for(int j=0; j<arr[i].length;j++) {
+                    System.out.print(arr[i][j]+" ") ;
+                }
+                System.out.println();
+            }
+
+        }    
+
+     public static int[] rotate(int[] arr, int Ktimes){
+        // System.out.print("Ktimes---"+Ktimes) ;
+        for(int i=1;i<=Ktimes;i++){
+            int j = 0 ;
+            int last = arr[0];
+            for(j=0;j<arr.length-1;j++){
+                arr[j] = arr[j+1] ;
+            }
+            arr[j] = last;
         }
-        arr[0] = last;
-
-
+        return arr;
     }
 }
